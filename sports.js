08f6abde7764d0fab1ed19af1f1cd2e595dbaf13
Abhishek Pandit <86287654+abhishek-pandit1@users.noreply.github.com>
2022-11-08 18:49:03 +0530

    all files are successfully added.
    completed 1.

---
+++ sports.js
@@ -0,0 +1,47 @@
+
+// grab the container 
+let newsHtml = "";
+let accordion = document.getElementById('accordion');
+
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=e703961b2d7f4b5daeaf20c1a411ef3d", true);
+
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        // console.log(json);
+        let articles = json.articles;
+        // console.log(articles);
+        // for (news in articles) {
+        articles.forEach(function (element, index) {
+            news = `<div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="flush-heading${index}">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapse${index}" aria-expanded="false" aria-controls="flush-collapse${index}">
+                  <span style="color: #404040 ;"> ${element["title"]} </span>
+                </button>
+              </h2>
+              <div id="flush-collapse${index}" class="accordion-collapse collapse" aria-labelledby="flush-heading${index} style="padding: 30px;">
+                <div class="accordion-body" >${element["content"]}
+                  <a href="${element["url"]}" target="_blank">Read more..</a>
+                </div>
+              </div>
+            </div>
+          `;
+            newsHtml += news;
+
+        });
+        accordion.innerHTML = newsHtml;
+
+    }
+    else {
+        console.log('some error occured');
+    }
+}
+xhr.send();
+
+
+
+
